3415a9094dfe17ca96935d1546e495a2abb410a0
André Escobar <andre.escobar@blintz.com.br>
2024-05-18 19:46:10 -0300

    chore: improve readme

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
-  mode: 'production',
-  entry: './app.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'final.js',
-  },
-  target: 'node',
-  externals: [nodeExternals()],
-};
+    mode: 'production',
+    entry: './app.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'final.js',
+    },
+    target: 'node',
+    externals: [nodeExternals()],
+}
